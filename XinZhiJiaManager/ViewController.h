1cf397e9c02f7ef4b0a77b2d514a6e82ac5cc5a3
cjx <cjx@caojiaxindemacbook-pro.local>
2017-04-10 09:44:09 +0800

    Initial Commit

---
+++ XinZhiJiaManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XinZhiJiaManager
+//
+//  Created by cjx on 2017/4/10.
+//  Copyright © 2017年 XZL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XinZhiJiaManagerTests/XinZhiJiaManagerTests.m
@@ -0,0 +1,39 @@
+//
+//  XinZhiJiaManagerTests.m
+//  XinZhiJiaManagerTests
+//
+//  Created by cjx on 2017/4/10.
+//  Copyright © 2017年 XZL. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XinZhiJiaManagerTests : XCTestCase
+
+@end
+
+@implementation XinZhiJiaManagerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
